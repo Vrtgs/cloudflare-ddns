f2b02caddee87c7165933758eff5dec3dfc1db27
Vrtgs <mirza123.the.best@gmail.com>
2024-04-30 14:11:34 +0300

    new sys_common with new path resolution

--- src/network_listener/sys/mod.rs
+++ src/network_listener/sys/mod.rs
@@ -1,21 +1,15 @@
-#[cfg(windows)]
-mod windows;
+#[cfg_attr(windows, path = "windows.rs")]
+#[cfg_attr(target_os = "macos", path = "macos.rs")]
+mod sys_common;
 
 use std::convert::Infallible;
-#[cfg(windows)]
-use windows as sys;
-
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-use macos as sys;
-
-pub use sys::has_internet;
-
 use crate::updaters::UpdatersManager;
+
+
+pub use sys_common::has_internet;
 pub fn subscribe(updaters_manager: &mut UpdatersManager) -> Result<(), Infallible> {
     let (updater, jh_entry) = updaters_manager.add_updater("network-listener");
-    jh_entry.insert(sys::subscribe(updater));
+    jh_entry.insert(sys_common::subscribe(updater));
 
     Ok(())
 }
