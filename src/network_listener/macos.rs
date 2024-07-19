fb5e03570f21606c81f9014a004be646a51d2c74
vrtgs <mirza123.the.best@gmail.com>
2024-07-19 04:54:21 +0300

    Fix macos.rs

--- src/network_listener/macos.rs
+++ src/network_listener/macos.rs
@@ -1,15 +1,16 @@
 #![cfg(target_os = "macos")]
 
+use std::convert::Infallible;
 use crate::dbg_println;
 use crate::updaters::Updater;
-use std::future::Future;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::time::Duration;
 use system_configuration::network_reachability::{
     ReachabilityFlags, SCNetworkReachability, SchedulingError, SetCallbackError,
 };
-use tokio::runtime::Handle as TokioHandle;
 use tokio::sync::Notify;
 use tokio::task::JoinHandle;
+use crate::util::new_skip_interval_after;
 
 #[derive(thiserror::Error, Debug)]
 pub enum UpdaterError {
@@ -34,23 +35,8 @@ fn has_internet_from_flags(flags: ReachabilityFlags) -> bool {
             || flags.contains(ReachabilityFlags::IS_WWAN))
 }
 
-fn listen<F: Fn() + Sync, S: Future>(
-    _notify_callback: F,
-    shutdown: S,
-) -> Result<S::Output, UpdaterError> {
-    // let mut sc = SCNetworkReachability::from(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0));
-    // sc.set_callback(|flags| {
-    //     if has_internet_from_flags(flags) {
-    //         notify_callback()
-    //     }
-    // })?;
-    // unsafe { sc.schedule_with_runloop(&CFRunLoop::get_current(), kCFRunLoopDefaultMode)?; };
-
-    Ok(TokioHandle::current().block_on(shutdown))
-}
-
 pub fn subscribe(updater: Updater) -> JoinHandle<()> {
-    tokio::task::spawn_blocking(move || {
+    tokio::task::spawn(async move {
         let local_notify = Notify::new();
         let callback = || {
             dbg_println!("Network Listener: got network update!");
@@ -58,15 +44,29 @@ pub fn subscribe(updater: Updater) -> JoinHandle<()> {
                 local_notify.notify_waiters();
             }
         };
-
-        let shutdown = async {
-            tokio::select! {
-                _ = local_notify.notified()  => (),
-                _ = updater.wait_shutdown() => ()
+        
+        let listen_loop = async move {
+            let mut timer = new_skip_interval_after(Duration::from_secs(30));
+            let mut last: bool = has_internet().await;
+            loop {
+                timer.tick().await;
+                let new = has_internet().await;
+                if last != new { 
+                    last = new;
+                    callback()
+                }
             }
         };
+        
+        tokio::select! {
+            never = listen_loop => {
+                let never: Infallible = never;
+                match never {}
+            },
+            _ = local_notify.notified()  => (),
+            _ = updater.wait_shutdown() => ()
+        }
 
-        let res = listen(callback, shutdown);
-        updater.exit(res)
+        updater.exit(Ok::<_, Infallible>(()))
     })
 }
