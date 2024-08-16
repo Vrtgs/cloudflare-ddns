a8d406472656d0f42e8e7feb07c33c3ef87055f1
Ali AlSalman <alireza.salman@icloud.com>
2024-08-16 03:34:44 +0300

    fix: double-clicking exe on macOS

--- src/pre.rs
+++ src/pre.rs
@@ -20,8 +20,22 @@ fn ensure_root() {
     }
 }
 
+#[cfg(target_os = "macos")]
+fn set_working_dir() {
+    use std::env;
+
+    env::current_exe()
+        .and_then(|mut path| {
+            path.pop();
+            env::set_current_dir(&path)
+        })
+        .unwrap_or_else(|e| crate::abort!("{e}"));
+}
 pub fn pre_run() {
     err::set_hook();
     #[cfg(target_os = "linux")]
     ensure_root();
+
+    #[cfg(target_os = "macos")]
+    set_working_dir();
 }
